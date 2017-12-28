9d6816a022e3e0cf8b8888c34906947272ceb4cc
kogito <kogito@lenovo>
2017-12-28 18:41:57 +0100

    add main script

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 03:14:54 2017
+@author: kogito
+"""
+from Gui import MainWindow
+
+
+def main():
+    MainWindow('Texhide')
+
+if __name__ == '__main__':
+    main()
